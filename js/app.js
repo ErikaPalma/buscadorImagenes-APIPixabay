2c107bb953da1d73d4dab312b489829589614f68
Erika <erikapalmasan@gmail.com>
2022-04-25 18:51:57 +0200

    Mostrando los resultados de la consulta - Retoques en la apariencia

--- js/app.js
+++ js/app.js
@@ -57,5 +57,32 @@ function buscarImagenes(termino) {
 }
 
 function mostrarImagenes(imagenes) {
-  console.log(imagenes);
+  limpiarHTML();
+
+  //Recorrer el array de imágenes
+
+  imagenes.forEach((imagen) => {
+    const { previewURL, likes, views, largeImageURL, user } = imagen;
+
+    resultado.innerHTML += `
+        <div class="w-1/2  md:w-1/3 lg:w-1/4 p-3 mb-4">
+            <div class="bg-white">
+                <img class="w-full" src="${previewURL}">
+                <div class="p-4">
+                    <p class="font-bold">${likes}<span class="font-light"> Me gusta</span></p>
+                    <p class="font-bold">${views}<span class="font-light"> veces visto</span></p>
+                    <p class="font-bold">By ${user}</p>
+                    <button class="btn"><a href="${largeImageURL}" target="_blank" rel="noopener noreferrer">Ver imagen HD</a></button>
+                </div>
+            </div>
+        </div>
+     `;
+  });
+  //Mostrar el HTML
+}
+
+function limpiarHTML() {
+  while (resultado.firstChild) {
+    resultado.removeChild(resultado.firstChild);
+  }
 }
