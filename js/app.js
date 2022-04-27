4743fc5222f105c079617fc9a785d523d63cd678
Erika <erikapalmasan@gmail.com>
2022-04-27 18:57:23 +0200

    Paginación: creando un generador

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 const formulario = document.querySelector("#formulario");
 const resultado = document.querySelector("#resultado");
 
+const registrosPorPagina = 40;
+let totalPaginas;
+let iterador;
+
 window.onload = () => {
   formulario.addEventListener("submit", validarFormulario);
 };
@@ -47,13 +51,26 @@ function mostrarAlerta(mensaje) {
 
 function buscarImagenes(termino) {
   const key = "27000774-24f141e29c282119bae780cea";
-  const url = `https://pixabay.com/api/?key=${key}&q=${termino}&image_type=photo`;
+  const url = `https://pixabay.com/api/?key=${key}&q=${termino}&image_type=photo&per_page=100`;
 
   fetch(url)
     .then((respuesta) => respuesta.json())
     .then((resultado) => {
+      totalPaginas = calcularPaginas(resultado.totalHits);
+      console.log(totalPaginas);
       mostrarImagenes(resultado.hits);
     });
+}
+
+//Generador que registra cantidad de elementos según páginas
+function* crearPaginador(total) {
+  for (let i = 1; i <= total; i++) {
+    yield i;
+  }
+}
+
+function calcularPaginas(total) {
+  return parseInt(Math.ceil(total / registrosPorPagina));
 }
 
 function mostrarImagenes(imagenes) {
@@ -62,12 +79,12 @@ function mostrarImagenes(imagenes) {
   //Recorrer el array de imágenes
 
   imagenes.forEach((imagen) => {
-    const { previewURL, likes, views, largeImageURL, user } = imagen;
+    const { likes, views, largeImageURL, user } = imagen;
 
     resultado.innerHTML += `
         <div class="w-1/2  md:w-1/3 lg:w-1/4 p-3 mb-4">
             <div class="bg-white">
-                <img class="w-full" src="${previewURL}">
+                <img class="w-full" src="${largeImageURL}">
                 <div class="p-4">
                     <p class="font-bold">${likes}<span class="font-light"> Me gusta</span></p>
                     <p class="font-bold">${views}<span class="font-light"> veces visto</span></p>
@@ -78,11 +95,15 @@ function mostrarImagenes(imagenes) {
         </div>
      `;
   });
-  //Mostrar el HTML
+  imprimirPaginador();
 }
 
 function limpiarHTML() {
   while (resultado.firstChild) {
     resultado.removeChild(resultado.firstChild);
   }
+}
+
+function imprimirPaginador() {
+  iterador = crearPaginador(totalPaginas);
 }
